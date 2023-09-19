372fa4703dc12e1837dbacc7087d205ed5d11181
H. Franc <84089238+betcorg@users.noreply.github.com>
2023-09-19 01:39:16 -0600

    Update spotdl.sh

--- spotdl.sh
+++ spotdl.sh
@@ -3,13 +3,11 @@
 cd ~
 echo -e "\n\e[1;32m[*] Actualizando repositorios\e[0m\n" 
 sleep 3 
-apt update -y
-apt upgrade -y
+apt update && apt upgrade -y
 echo -e "\n\e[1;32m[*] Instalando dependencias\e[0m\n" 
 sleep 3 
-apt install binutils build-essential python python3 ffmpeg
+apt install binutils build-essential rust python python3 ffmpeg curl
 pip install setuptools
 echo -e "\n\e[1;32m[*] Instalando spotDL\e[0m\n" 
 sleep 3 
 curl -L https://raw.githubusercontent.com/spotDL/spotify-downloader/master/scripts/termux.sh | sh
-echo -e "\n\e[1;32m[*]spotDL instalado con éxito\e[0m\n" 
